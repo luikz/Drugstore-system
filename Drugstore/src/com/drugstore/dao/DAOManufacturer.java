aec273e9bfb38b262a7531116ad8591f9e07e18b
Luiz Carlos <luizcarlos--1@hotmail.com>
2019-06-04 13:23:52 -0300

    Drugstore files

--- Drugstore/src/com/drugstore/dao/DAOManufacturer.java
+++ Drugstore/src/com/drugstore/dao/DAOManufacturer.java
@@ -7,111 +7,128 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.drugstore.domain.Manufacturer;
-import com.drugstore.factory.FactoryConnection;
+import com.drugstore.factory.ConnectionFactory;
 
-public class DAOManufacturer {  
+public class DAOManufacturer {
 	public void save(Manufacturer m) throws SQLException {
 		String sql = "INSERT INTO manufacturer (description) VALUES(?)";
-		
-		Connection connection = FactoryConnection.connect();
-		
+
+		Connection connection = ConnectionFactory.connect();
+
 		PreparedStatement ps = connection.prepareStatement(sql);
 		ps.setString(1, m.getDescription());
 		ps.executeUpdate();
 	}
-	
-	public ArrayList<Manufacturer> list() throws SQLException {	
+
+	public ArrayList<Manufacturer> list() throws SQLException {
 		String sql = "SELECT id, description FROM manufacturer ORDER BY description ASC";
-		
-		Connection connection = FactoryConnection.connect();
-		
+
+		Connection connection = ConnectionFactory.connect();
+
 		PreparedStatement ps = connection.prepareStatement(sql);
-		
+
 		ResultSet rs = ps.executeQuery();
-		
+
 		ArrayList<Manufacturer> manufacturerList = new ArrayList<Manufacturer>();
-		
-		while(rs.next()) {
+
+		while (rs.next()) {
 			Manufacturer m = new Manufacturer();
 			m.setId(rs.getLong("id"));
 			m.setDescription(rs.getString("description"));
 			manufacturerList.add(m);
 		}
+		
+		connection.close();
+		ps.close();
+		rs.close();
 		return manufacturerList;
 	}
-	
+
 	public Manufacturer searchById(Manufacturer m) throws SQLException {
 		String sql = "SELECT id, description FROM manufacturer WHERE id = ?";
-		
-		Connection connection = FactoryConnection.connect();
-		
+
+		Connection connection = ConnectionFactory.connect();
+
 		PreparedStatement ps = connection.prepareStatement(sql);
 		ps.setLong(1, m.getId());
-		
+
 		ResultSet rs = ps.executeQuery();
-		
-		Manufacturer searchingResult= null;
-		
-		if(rs.next()) {
+
+		Manufacturer searchingResult = null;
+
+		if (rs.next()) {
 			searchingResult = new Manufacturer();
 			searchingResult.setId(rs.getLong("id"));
 			searchingResult.setDescription(rs.getString("description"));
 		}
+		connection.close();
+		ps.close();
+		rs.close();
+		
 		return searchingResult;
 	}
-	
+
 	public ArrayList<Manufacturer> searchByDescription(Manufacturer m) throws SQLException {
-		String sql = "SELECT id, description FROM manufacturer WHERE description = ? "
-				+ "ORDER BY description ASC";
-		
-		Connection connection = FactoryConnection.connect();
-		
+		String sql = "SELECT id, description FROM manufacturer WHERE description = ? " + "ORDER BY description ASC";
+
+		Connection connection = ConnectionFactory.connect();
+
 		PreparedStatement ps = connection.prepareStatement(sql);
 		ps.setString(1, "%" + m.getDescription() + "%");
-		
+
 		ResultSet rs = ps.executeQuery();
-		
+
 		ArrayList<Manufacturer> manufacturerList = new ArrayList<>();
-		
-		while(rs.next()) {
+
+		while (rs.next()) {
 			Manufacturer manu = new Manufacturer();
 			manu.setId(rs.getLong("id"));
 			manu.setDescription("description");
-			
+
 			manufacturerList.add(manu);
 		}
+		
+		connection.close();
+		ps.close();
+		rs.close();
 		return manufacturerList;
 	}
-	
+
 	public void update(Manufacturer m) throws SQLException {
 		String sql = "UPDATE manufacturer SET description = ? WHERE id = ?";
-		
-		Connection connection = FactoryConnection.connect();
-		
+
+		Connection connection = ConnectionFactory.connect();
+
 		PreparedStatement ps = connection.prepareStatement(sql);
 		ps.setString(1, m.getDescription());
 		ps.setLong(2, m.getId());
 		ps.executeUpdate();
+		
+		connection.close();
+		ps.close();
 	}
-	
+
 	public void delete(Manufacturer m) throws SQLException {
 		String sql = "DELETE FROM manufacturer where id = ?";
-		
-		Connection connection = FactoryConnection.connect();
-		
+
+		Connection connection = ConnectionFactory.connect();
+
 		PreparedStatement ps = connection.prepareStatement(sql);
 		ps.setLong(1, m.getId());
 		ps.executeUpdate();
+		
+		connection.close();
+		ps.close();
 	}
-	
+
 	public static void main(String[] args) {
-		//Save
+		// Save
 		Manufacturer m1 = new Manufacturer();
 //		m1.setDescription("DESCRIPTION 1");
-		
-		Manufacturer m2 = new Manufacturer();
+
+//		Manufacturer m2 = new Manufacturer();
 //		m2.setDescription("DESCRIPTION 2");
-		
+
 		DAOManufacturer dm = new DAOManufacturer();
 //		try {
 //			dm.save(m1);
@@ -121,8 +138,8 @@ public class DAOManufacturer {
 //			System.out.println("Manufactures weren't updated successfully!");
 //			e.printStackTrace();
 //		}
-		
-		//List
+
+		// List
 //		try {
 //			ArrayList<Manufacturer> manufacturerList = dm.list();
 //			
@@ -133,8 +150,8 @@ public class DAOManufacturer {
 //			System.out.println("Occured error while trying to list the manufactures!");
 //			e.printStackTrace();
 //		}
-		
-		//Searching by id
+
+		// Searching by id
 //		m1.setId(3L);
 //		m2.setId(5L);	
 //		
@@ -148,22 +165,22 @@ public class DAOManufacturer {
 //			System.out.println("Couldn't find any manufacturer by id!");
 //			e.printStackTrace();
 //		}
-		
-		//Searching by description
+
+		// Searching by description
 		m1.setDescription("3");
-		
+
 		try {
 			ArrayList<Manufacturer> manufacturerList = dm.searchByDescription(m1);
-			
-			for(Manufacturer m: manufacturerList) {
+
+			for (Manufacturer m : manufacturerList) {
 				System.out.println("Result: " + m);
 			}
 		} catch (SQLException e) {
 			System.out.println("Couldn't find any manufacturer by description!");
 			e.printStackTrace();
 		}
-		
-		//Update
+
+		// Update
 //		m1.setId(4L);
 //		m1.setDescription("Description 4");
 //		
@@ -174,8 +191,8 @@ public class DAOManufacturer {
 //			System.out.println("Manufactures were not updated!");
 //			e.printStackTrace();
 //		}
-		
-		//Delete
+
+		// Delete
 //		m1.setId(1L);
 //		m2.setId(2L);
 //		
